affdcfca88481619c658f6e1476d550d5c2e284f
andreroche <36365088+andreroche@users.noreply.github.com>
2018-02-11 12:40:42 +0000

    Create printfunction.py

---
+++ printfunction.py
@@ -0,0 +1,9 @@
+print ('Hello World')
+# this is a test program 
+print('goodbye world')
+print (''' this is a multi line string. this is the first line.
+this is the second line.
+this is the third.
+this is the fourth''')
+print (100*100 +25E10)
+print (10*10)
