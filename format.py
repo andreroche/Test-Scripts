4ea9419907ff0339dc4091fa656bbd437c747408
andreroche <36365088+andreroche@users.noreply.github.com>
2018-02-11 12:42:08 +0000

    Create format.py

---
+++ format.py
@@ -0,0 +1,9 @@
+age = 37
+name = 'Bob'
+gender = 'male'
+hobby = 'cycling'
+timeofday = 'at night'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with python?'.format(name))
+print('{4} {2} {1} was {3} when he was {0}'.format(timeofday,gender,name,hobby,age))
