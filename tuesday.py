2b6b45486f2dd0946949f0d9f450f2f315c7c012
andreroche <36365088+andreroche@users.noreply.github.com>
2018-02-11 12:35:19 +0000

    Create tuesday.py

---
+++ tuesday.py
@@ -0,0 +1,10 @@
+# André Roche - This is a test to see if it is tuesday
+
+# Is it Tuesday?
+
+import datetime # this is importing a library
+
+if datetime.datetime.today().weekday() == 1: # this is just a format taken from stack overflow on the net
+    print("yay! It is Tuesday.")
+else:
+    print("Unfortunately it is not Tuesday") 
