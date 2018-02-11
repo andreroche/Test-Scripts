6eee8bcbbff32ce576bd584fb21cf4c6dc301ed0
andreroche <36365088+andreroche@users.noreply.github.com>
2018-02-11 12:41:25 +0000

    Create format3.py

---
+++ format3.py
@@ -0,0 +1,17 @@
+age = 37
+name = 'Bob'
+gender = 'male'
+hobby = 'cycling'
+timeofday = 'at night'
+typeofbike = 'giant'
+country = 'ireland'
+sizeofwheels = '700'
+
+
+print('{} {} {} was {} when he was {}'.format(timeofday,gender,name,hobby,age))
+print('the sun is shining in the sky during the day')
+print('{} flew to {} then bought a {} bike It had {} wheels'.format(name,country,typeofbike,sizeofwheels))
+print('700 point what ? exactly how wide were {}s wheels'.format(name))
+print('bobs wheels were {0:.3f}'.format(700))
+print('how wide were they?')
+print('they were {0:.3f} CM'.format(25))
